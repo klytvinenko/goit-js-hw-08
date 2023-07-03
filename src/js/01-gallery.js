f2cefc87e92cd13719b177b8e84b494c6e5d7a60
klytvinenko <58306149+klytvinenko@users.noreply.github.com>
2023-07-03 22:11:18 +0300

    second

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,42 +1,31 @@
-// Add imports above this line
 import SimpleLightbox from 'simplelightbox';
-import { galleryItems } from './gallery-items';
 import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
 
 // Change code below this line
-const createRenderGallery =
- renderGallery(galleryItems);
 
-galleryContainer.insertAdjacentHTML(
- "beforeend",
- createRenderGallery
-);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
-function renderGallery(galleryItems) {
- return galleryItems
-  .map(({ preview, original, description }) => {
-   return `<li class='gallery__item'>
-      <a class='gallery__link' href='${original}'>
-      <img
-        class='gallery__image'
-        src='${preview}'
-        alt='${description}'/>
-      </a>
-      </li>`;
-  })
-  .join("");
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`
+  }).join('');
 }
 
+// use library SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
 
-const lightbox = new SimpleLightbox(
- ".gallery__link",
- {
-  captionsData: "alt",
-  caption: true,
-  captionPosition: "bottom",
-  captionDelay: 250,
-  
- }
-);
-console.log(galleryItems);
-console.log(SimpleLightbox);
