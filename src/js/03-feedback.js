9a5e4e13e2eb21c9185927213da00aa5bd26cec0
klytvinenko <58306149+klytvinenko@users.noreply.github.com>
2023-07-05 19:28:36 +0300

    new commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,78 @@
 // 
 import throttle from 'lodash.throttle';
+
+
+
+// const form = document.querySelector('.feedback-form');
+// const btn = document.querySelector('button');
+// const LS_KEY = 'feedback-form-state';
+// let obj = {};
+
+// console.log('ok');
+
+// form.addEventListener('input', throttle(onIput, 500));
+// form.addEventListener('submit', onSubmit);
+
+// obj = JSON.parse(localStorage.getItem(LS_KEY)) ?? {};
+// fillForm();
+// function fillForm() {
+//     if(obj) {
+//         form.elements.email.value = obj.email;
+//         form.elements.message.value = obj.message;
+//     }
+// }
+
+// function onIput (evt) {
+//     obj[evt.target.name] = evt.target.value;
+    
+//     localStorage.setItem(LS_KEY, JSON.stringify(obj));
+// }
+
+// function onSubmit(evt) {
+//     evt.preventDefault();
+//     console.log(obj);
+
+//     if(localStorage.getItem(LS_KEY)) {
+//         localStorage.removeItem(LS_KEY);
+//     }
+
+//     evt.currentTarget.reset();
+//     obj = {};
+// }
+
+
 const form = document.querySelector('.feedback-form');
-const btn = document.querySelector('button');
-const LS_KEY = 'feedback-form-state';
-let obj = {};
 
-console.log('ok');
-
-form.addEventListener('input', throttle(onIput, 500));
-form.addEventListener('submit', onSubmit);
-
-obj = JSON.parse(localStorage.getItem(LS_KEY)) ?? {};
+form.addEventListener('input',throttle(inputForm,500));
+form.addEventListener('submit',submitForm);
+let formValues=JSON.parse(localStorage.getItem(STORAGE_KEY));
 fillForm();
 function fillForm() {
-    if(obj) {
-        form.elements.email.value = obj.email;
-        form.elements.message.value = obj.message;
+    if(formValues) {
+        form.elements.email.value=formValues.email;
+        form.elements.message.value=formValues.message;
     }
 }
+function inputForm() {
 
-function onIput (evt) {
-    obj[evt.target.name] = evt.target.value;
-    
-    localStorage.setItem(LS_KEY, JSON.stringify(obj));
-}
-
-function onSubmit(evt) {
-    evt.preventDefault();
-    console.log(obj);
-
-    if(localStorage.getItem(LS_KEY)) {
-        localStorage.removeItem(LS_KEY);
+    formValues={
+        email:form.elements.email.value,
+        message:form.elements.message.value
+    }
+    try{
+        localStorage.setItem(STORAGE_KEY,JSON.stringify(formValues));
+    }catch(error){
+        console.error(error.message)
     }
 
-    evt.currentTarget.reset();
-    obj = {};
 }
-
+function submitForm(event) {
+    event.preventDefault();
+    console.log(formValues);
+    localStorage.removeItem(STORAGE_KEY);
+    event.currentTarget.reset();
+    formValues={};
+}
 
 
 // const LOCAL_KEY = 'feedback-form-state';
